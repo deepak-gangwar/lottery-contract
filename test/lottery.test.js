39864b0efc1612bc8eb1ec74241c94352298e037
Deepak Gangwar <56259752+deepak-gangwar@users.noreply.github.com>
2022-02-22 19:44:00 +0530

    Test for successful reset at end.

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -124,5 +124,27 @@ describe('Lottery Contract', () => {
             assert(err);
         }
     });
+    
+    // End to end test
+    it('sends money to the winner and resets the players array', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('2', 'ether')
+        });
+
+        // Using getBalance function of web3 to know the 
+        // balance of contract or any individual account
+        
+        const initialBalance = await web3.eth.getBalance(accounts[0]);
+        await lottery.methods.pickWinner().send({ from: accounts[0] });
+        const finalBalance = await web3.eth.getBalance(accounts[0]);
+        
+        //Technically the difference between initial and final balance
+        // should be 2 ether, however we also spend some ether on gas
+        // so in actual it should be less than that
+
+        const difference = finalBalance - initialBalance;
+        assert(difference > web3.utils.toWei('1.8', 'ether'));
+    });
 });
 
